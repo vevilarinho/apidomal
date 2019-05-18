f38944b033a1acc0e42070289f4ed86c3e224a1c
ALEXSANDRO FERNANDES SARTORI ROLIANO <ALU0810510@uniritter.local>
2019-05-18 16:48:01 -0300

    cria model

---
+++ apidomal/src/main/java/com/veronicavilarinho/apidomal/models/Contact.java
@@ -0,0 +1,57 @@
+package com.veronicavilarinho.apidomal.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Contact {
+
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String phone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Contact(Long id, String name, String email, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
+	
+
+}

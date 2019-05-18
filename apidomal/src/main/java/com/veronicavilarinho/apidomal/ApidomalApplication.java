67d4194f3e9c70fe47c849b227e5211291d46224
ALEXSANDRO FERNANDES SARTORI ROLIANO <ALU0810510@uniritter.local>
2019-05-18 15:38:43 -0300

    envia ao repositorio local

---
+++ apidomal/src/main/java/com/veronicavilarinho/apidomal/ApidomalApplication.java
@@ -0,0 +1,13 @@
+package com.veronicavilarinho.apidomal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidomalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidomalApplication.class, args);
+	}
+
+}
